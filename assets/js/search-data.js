842e34d5b9e0593a1776533c9f6ada75791b2e87
vitercik <34546473+vitercik@users.noreply.github.com>
2026-05-17 21:35:54 +0000

    Deploying to gh-pages from @ vitercik/vitercik.github.io@1bbabd27ad89ddb4c0222fa0cf24dd002d4480da 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/group/";
           },
+        },{id: "nav-talks",
+          title: "talks",
+          description: "Recent talks",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/talks/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Teaching at Stanford",
